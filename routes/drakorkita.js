a9ec1b370a604a9b366586a04d7a7af6b0dc426b
Rukmana <maakunn470@gmail.com>
2026-01-07 20:57:06 +0700

    Refactor and add debug/test routes in drakorkita.js
    Refactor routes in drakorkita.js for better organization and add debug and test routes.

--- routes/drakorkita.js
+++ routes/drakorkita.js
@@ -1,6 +1,6 @@
-const { Router } = require("express")
-
-const router = Router()
+// routes/drakorkita.js
+const { Router } = require("express");
+const router = Router();
 
 const {
     seriesAll,
@@ -13,20 +13,26 @@ const {
     detailGenres,
     searchAll,
     detailAllType,
-    getVideoUrl
+    getVideoUrl,
+    debugEnv,
+    testConnection
 } = require("../controllers/drakorkita");
 
-// drakorkita
-router.get("/series", seriesAll)
-router.get("/movie", movieAll)
-router.get("/series/updated", seriesUpdated)
-router.get("/series/ongoing", ongoingSeries)
-router.get("/series/completed", completedSeries)
-router.get("/movie/newest", newMovie)
-router.get("/genres", genres)
-router.get("/genres/:endpoint", detailGenres)
-router.get("/search", searchAll)
-router.get("/detail/:endpoint", detailAllType)
+// API Routes
+router.get("/series", seriesAll);
+router.get("/movie", movieAll);
+router.get("/series/updated", seriesUpdated);
+router.get("/series/ongoing", ongoingSeries);
+router.get("/series/completed", completedSeries);
+router.get("/movie/newest", newMovie);
+router.get("/genres", genres);
+router.get("/genres/:endpoint", detailGenres);
+router.get("/search", searchAll);
+router.get("/detail/:endpoint", detailAllType);
 router.get("/video/:endpoint", getVideoUrl);
 
-module.exports = router
+// Debug Routes
+router.get("/debug", debugEnv);
+router.get("/test", testConnection);
+
+module.exports = router;
